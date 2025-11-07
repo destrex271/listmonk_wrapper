2d759a1733e4bbbdf4ecc3cbd821d359aa1bad48
Akshat Jaimini <destrex271@gmail.com>
2025-11-07 20:23:40 +0530

    add polling function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	// "encoding/base64"
 
 	"bytes"
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -36,163 +37,70 @@ var (
 	englishListId3m, _ = strconv.Atoi(os.Getenv("ENGLISH_LIST_3M"))
 	hindiListId1m, _   = strconv.Atoi(os.Getenv("HINDI_LIST_1M"))
 	englishListId1m, _ = strconv.Atoi(os.Getenv("ENGLISH_LIST_1M"))
+	database_url       = os.Getenv("DATABASE_URL")
 )
 
-func operate(listName string, campaign Postback, messenger string) error {
-	// Create Temporary List
-	listId, err := CreateNewList(listEndpoint, apiUsername, accessToken, membershipEndpoint, listName)
-	if err != nil {
-		return err
+func syncSubscribers() {
+	conn, err := pgx.Connect(context.Background(), database_url)
+	if err != nil{
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
-	log.Println("Created list with ID", listId)
-	// Get Recepients IDs
-	ids, err := FetchIDsFromUUIDs(apiUsername, accessToken, subsEndpoint, campaign.Recipients)
-	if err != nil {
-		return err
-	}
-	log.Println("Got recepient ids as: ", ids)
-	// Add Recepients to List
-	err = UpdateRecepients(apiUsername, accessToken, membershipEndpoint, ids, listId, "add")
-	if err != nil {
-		return err
-	}
-	log.Println("Added recepeients to List")
+	defer conn.Close(context.Background())o
 
-	// Create Campaign with new List
-	lId, err := strconv.Atoi(listId)
-	if err != nil {
-		return err
-	}
-	campaign.Campaign.Name += "_" + listName
-	cpId, err := SendCapmaign(campaignEndpoint, apiUsername, accessToken, campaign, lId, messenger)
-	if err != nil {
-		return err
+	move_verified := "SELECT mark_verified_on_view();"
+	check_bounce_threshold := "SELECT check_bounce_threshold();"
+	sync_subs := "SELECT sync_all_subscribers_to_verified_status();"
+
+	// convert status of unverified to verified if viewed
+	_, err = conn.Exec(context.Background(), move_verified)
+	if err != nil{
+		fmt.Println("%v", err)
+		os.Exit(1)
 	}
 
-	log.Println("Sent campaign", cpId)
-	// log.Println("Polling status....")
+	// convert status of verified to unverified if bounced
+	_, err = conn.Exec(context.Background(), check_bounce_threshold)
+	if err != nil{
+		fmt.Println("%v", err)
+		os.Exit(1)
+	}
 
-	// for !GetCampaignStatus(cpId, campaignEndpoint, apiUsername, accessToken) {
-	// 	log.Println("Campaign still running...")
-	// 	time.Sleep(time.Second * 10)
-	// }
-	// log.Println("Campaign sent.., time for cleanup")
+	// TODO: For any bounced subs that need to be deleted, take
+	// action on ASP Server DB
 
-	// // Remove Recepients from List
-	// err = UpdateRecepients(apiUsername, accessToken, membershipEndpoint, ids, listId, "remove")
-	// if err != nil {
-	// 	return err
-	// }
+	// take action on verif and unverif by moving them around lists
+	_, err = conn.Exec(context.Background(), sync_subs)
+	if err != nil{
+		fmt.Println("%v", err)
+		os.Exit(1)
+	}
 
-	// // Wait till status is compeleted
-
-	// // Delete the List
-	// err = DeleteList(listEndpoint, apiUsername, accessToken, listId)
-	// if err != nil {
-	// 	return err
-	// }
-
-	return nil
 }
 
-func proxyHandler_RoutingMessenger(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var data Postback
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		http.Error(w, "Invalid request from JSON", http.StatusBadRequest)
-		return
-	}
-
-	var smtp_verified []Recipient
-	var smtp_unverified []Recipient
-
-	for _, sub := range data.Recipients {
-		if sub.Attribs["verification_status"] == true {
-			smtp_verified = append(smtp_verified, sub)
-		} else {
-			smtp_unverified = append(smtp_unverified, sub)
-		}
-	}
-
-	campaign1, campaign2 := data, data
-	campaign1.Recipients = smtp_verified
-	campaign2.Recipients = smtp_unverified
-
-	log.Println("VERIFIED: ", smtp_verified)
-	log.Println("UNVERIFIED: ", smtp_unverified)
-
-	if len(campaign1.Recipients) > 0 {
-		log.Println("Sending for verified....")
-		log.Println("Recps are: ", campaign1.Recipients)
-		fmt.Scanln()
-		err := operate("verified_"+campaign1.Campaign.Name, campaign1, "email-verified")
-		if err != nil {
-			log.Println("HELLO -.", err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+func pollCampaignStatus(id str){
+	// 4. Poll for completion
+	for {
+		time.Sleep(2 * time.Second)
+		reqCheck, _ := http.NewRequest("GET", fmt.Sprintf("%s/api/campaigns/%d", listmonkURL, id,created.Data.ID), nil)
+		reqCheck.Header.Set("Authorization", authHeader)
+		respCheck, err := http.DefaultClient.Do(reqCheck)
+		if err != nil || respCheck.StatusCode != 200 {
+			http.Error(w, "Failed to check campaign status " + err.Error(), http.StatusInternalServerError)
 			return
 		}
+		var statusResp struct {
+			Data CRCampaign `json:"data"`
+		}
+		json.NewDecoder(respCheck.Body).Decode(&statusResp)
+		respCheck.Body.Close()
+
+		if statusResp.Data.Status == "finished" {
+			break
+		}
 	}
-	// if len(campaign2.Recipients) > 0 {
-	// 	log.Println("Sending for unverified....")
-	// 	log.Println("Sending for verified....")
-	// 	err := operate("unverified_"+campaign2.Campaign.Name, campaign2, "email-unverified")
-	// 	if err != nil {
-	// 		log.Println("HELLO -.", err.Error())
-	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 		return
-	// 	}
-	// }
-	http.Error(w, "Successfully sent data", http.StatusOK)
 }
 
-func proxyHandler_ChangeList(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Parse request body
-	var data RequestData
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-
-	var req RequestBody
-	req.Email = data.Email
-
-	if data.Language == "hindi" {
-		if data.Frequency == "1" {
-			req.List1 = append(req.List1, hindiListId1m)
-		} else if data.Frequency == "3" {
-			req.List1 = append(req.List1, hindiListId3m)
-		}
-	} else if data.Language == "english" {
-		if data.Frequency == "1" {
-			req.List1 = append(req.List1, englishListId1m)
-		} else if data.Frequency == "3" {
-			req.List1 = append(req.List1, englishListId3m)
-		}
-	} else if data.Language == "both" {
-		if data.Frequency == "1" {
-			req.List1 = append(req.List1, englishListId1m)
-			req.List1 = append(req.List1, hindiListId1m)
-		} else if data.Frequency == "3" {
-			req.List1 = append(req.List1, englishListId3m)
-			req.List1 = append(req.List1, hindiListId3m)
-		}
-	}
-
-	fmt.Println(req.List1)
-
-	SendPreferenceChangeRequest(internalEndpoint, apiUsername, accessToken, req, w)
-}
 
 func withCORS(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -264,26 +172,7 @@ func proxyHandler_SendCampaign(w http.ResponseWriter, r *http.Request){
 	}
 	defer respStatus.Body.Close()
 
-	// 4. Poll for completion
-	for {
-		time.Sleep(2 * time.Second)
-		reqCheck, _ := http.NewRequest("GET", fmt.Sprintf("%s/api/campaigns/%d", listmonkURL, created.Data.ID), nil)
-		reqCheck.Header.Set("Authorization", authHeader)
-		respCheck, err := http.DefaultClient.Do(reqCheck)
-		if err != nil || respCheck.StatusCode != 200 {
-			http.Error(w, "Failed to check campaign status " + err.Error(), http.StatusInternalServerError)
-			return
-		}
-		var statusResp struct {
-			Data CRCampaign `json:"data"`
-		}
-		json.NewDecoder(respCheck.Body).Decode(&statusResp)
-		respCheck.Body.Close()
 
-		if statusResp.Data.Status == "finished" {
-			break
-		}
-	}
 
 	// 5. Respond success
 	w.Header().Set("Content-Type", "application/json")
@@ -292,7 +181,6 @@ func proxyHandler_SendCampaign(w http.ResponseWriter, r *http.Request){
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/proxy/change_user_pref", proxyHandler_ChangeList)
 	http.HandleFunc("/proxy/send_campaign", withCORS(proxyHandler_SendCampaign))
 
 	fmt.Println("Proxy server is running on port 8080")

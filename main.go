685ca3c9253d88c84605fa52844de66d6c75e2cc
Akshat Jaimini <destrex271@gmail.com>
2026-02-08 20:41:17 +0530

    fix

--- main.go
+++ main.go
@@ -583,28 +583,15 @@ func main() {
 
 	// Go-Routine to run blocklist job every 24 hours.
 	go func() {
-		ticker := time.NewTicker(time.Duration(blockListDropTime*2) * time.Hour)
-		defer ticker.Stop()
-
-		for {
-			log.Printf("Cron value is: %s\n", cronEnabled)
-			if cronEnabled != "1" {
-				continue
-			}
-			updateVerificationStatusOnSource()
-			<-ticker.C
-		}
-	}()
-
-	go func() {
 		ticker := time.NewTicker(time.Duration(blockListDropTime) * time.Hour)
 		defer ticker.Stop()
 
 		for {
-			log.Printf("Cron value is : %s for blocklist job\n", cronEnabled)
+			log.Printf("Cron value is : %s\n", cronEnabled)
 			if cronEnabled != "1" {
 				continue
 			}
+			updateVerificationStatusOnSource()
 			markBlockListInSource()
 			<-ticker.C
 		}
